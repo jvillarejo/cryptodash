b7ac0513125b9468d59e250b19fc4aa83f3b0c33
jvillarejo <arzivian87@gmail.com>
2013-12-11 11:16:17 -0300

    added bundler

--- cryptodash.rb
+++ cryptodash.rb
@@ -1,6 +1,8 @@
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + '/lib'))
 
+require 'rubygems' 
+require 'bundler/setup'
 require 'json'
 require 'chooser_repository'
 require 'account'
